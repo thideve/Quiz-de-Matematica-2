170f4c45d3412116c3b7e2acbf984897d5840d3c
thideve <107429305+thideve@users.noreply.github.com>
2022-09-28 20:54:04 -0300

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,13 @@
+function addUser(){
+   j1 = document.getElementById("j1").value;
+  
+  j2 = document.getElementById("j2").value;
+
+  localStorage.setItem("j1", j1);
+  localStorage.setItem("j2", j2);
+
+  window.location("game_page.html");
+
+  document.getElementById()
+}
+
